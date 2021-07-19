4e524a153613fe9bcb213f15649e08f506b9411f
sksat <sksat@sksat.net>
2021-07-20 08:39:39 +0900

    update: test exist check

--- src/test.rs
+++ src/test.rs
@@ -12,9 +12,17 @@ fn it_works() {
     let _whitelist: minecraft::UserList = serde_json::from_str(json).unwrap();
 }
 
-#[test]
-fn mc_yohane_su() {
+#[tokio::test]
+async fn mc_yohane_su() {
     let json = include_str!("../test/yohanesu_whitelist.json");
 
-    let _whitelist: minecraft::UserList = serde_json::from_str(json).unwrap();
+    let whitelist: minecraft::UserList = serde_json::from_str(json).unwrap();
+
+    for user in whitelist {
+        if user.exist().await.unwrap() {
+            continue;
+        }
+
+        panic!("\"{}\" does not exist!", user.name);
+    }
 }
